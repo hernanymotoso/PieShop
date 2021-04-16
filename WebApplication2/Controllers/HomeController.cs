708cd28a4df90558ffab120f820194698d95e1a2
Hernany <contatohernany@gmail.com>
2021-04-15 21:14:12 -0300

    Create HomeController and method Index

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) {
+            _pieRepository = pieRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ WebApplication2/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.ViewModels
+{
+    public class HomeViewModel
+    {
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+
+
+    }
+}
